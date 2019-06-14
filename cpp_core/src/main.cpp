3515c7b93d7807abb07a7318e5de63eaab80476a
James <BOJIT@users.noreply.github.com>
2019-06-14 12:35:40 +0100

    use local sysroot

--- cpp_core/src/main.cpp
+++ cpp_core/src/main.cpp
@@ -9,10 +9,10 @@
 int main(int argc, char* argv[])
 {	// Not required, but "correct" (see the SDK documentation)
 	std::cout << "hello world!" << std::endl;
-	/*if (!NDIlib_initialize())
+	if (!NDIlib_initialize())
 	{	// Cannot run NDI. Most likely because the CPU is not sufficient (see SDK documentation).
 		// you can check this directly with a call to NDIlib_is_supported_CPU()
 		printf("Cannot run NDI.");
 		return 0;
-	}*/
+	}
 }
