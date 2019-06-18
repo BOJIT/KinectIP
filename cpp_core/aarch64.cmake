a1caccc94d1a2019fbc0cf54750253a6cc581a94
James <BOJIT@users.noreply.github.com>
2019-06-18 19:47:34 +0100

    it works :):):)

--- cpp_core/aarch64.cmake
+++ cpp_core/aarch64.cmake
@@ -13,12 +13,17 @@ SET(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 #set(CMAKE_CXX_FLAGS "... --sysroot=/mnt/rock64/ ")
 #set(CMAKE_CXX_LINK_FLAGS "... --sysroot=/mnt/rock64/ ")
 
+#SET(CMAKE_LIBRARY_PATH /mnt/rock64/usr/local/lib)
+
 # where is the target environment
 SET(CMAKE_FIND_ROOT_PATH /mnt/rock64 )
-
+SET(CMAKE_SYSROOT /mnt/rock64 )
+#SET(CMAKE_PREFIX_PATH /mnt/rock64/ )
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+add_definitions(-Wall)
 # end of the file
