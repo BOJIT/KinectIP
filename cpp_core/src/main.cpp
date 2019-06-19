e22f139647195d3839ae25a86a08140f6801e370
James <BOJIT@users.noreply.github.com>
2019-06-19 18:11:51 +0100

    test patterns

--- cpp_core/src/main.cpp
+++ cpp_core/src/main.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, char* argv[])
 {	// Not required, but "correct" (see the SDK documentation)
-	std::cout << "hello world 2!" << std::endl;
+	std::cout << "NDI Test Patterns" << std::endl;
 	if (!NDIlib_initialize())
 	{	// Cannot run NDI. Most likely because the CPU is not sufficient (see SDK documentation).
 		// you can check this directly with a call to NDIlib_is_supported_CPU()
