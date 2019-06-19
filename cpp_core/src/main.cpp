82ddf46a47c8294c2d174ca90903209e21f3046f
James <BOJIT@users.noreply.github.com>
2019-06-19 15:33:59 +0100

    img remove

--- cpp_core/src/main.cpp
+++ cpp_core/src/main.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char* argv[])
 {	// Not required, but "correct" (see the SDK documentation)
-	std::cout << "hello world!" << std::endl;
+	std::cout << "hello world 2!" << std::endl;
 	if (!NDIlib_initialize())
 	{	// Cannot run NDI. Most likely because the CPU is not sufficient (see SDK documentation).
 		// you can check this directly with a call to NDIlib_is_supported_CPU()
@@ -19,21 +19,27 @@ int main(int argc, char* argv[])
 	// Lets load the file from disk
 	std::vector<unsigned char> png_data;
 	loadFile(png_data, "./img/NewTekNDI.png");
-	if (png_data.empty()) return 0;
-	
+	if (png_data.empty()) {
+		printf("No PNG Found");
+		return 0;
+	} 
 	// Decode the PNG file
 	std::vector<unsigned char> image_data;
 	unsigned long xres = 0, yres = 0;
-	if (decodePNG(image_data, xres, yres, &png_data[0], png_data.size(), true)) return 0;
-		
-	// Create an NDI source that is called "My PNG" and is clocked to the video.
+	if (decodePNG(image_data, xres, yres, &png_data[0], png_data.size(), true)) {
+		printf("Cannot Decode PNG \n");
+		return 0;
+	}	
+	// Create an NDI source that is clocked to the video.
 	NDIlib_send_create_t NDI_send_create_desc;
-	NDI_send_create_desc.p_ndi_name = "My PNG";
+	NDI_send_create_desc.p_ndi_name = "Colour";
 
 	// We create the NDI sender
 	NDIlib_send_instance_t pNDI_send = NDIlib_send_create(&NDI_send_create_desc);
-	if (!pNDI_send) return 0;
-
+	if (!pNDI_send) {
+		printf("Cannot Create NDI Sender \n");
+		return 0;
+	}
 	// We are going to create a frame
 	NDIlib_video_frame_v2_t NDI_video_frame;
 	NDI_video_frame.xres = xres;
@@ -47,7 +53,7 @@ int main(int argc, char* argv[])
 
 	// Lets measure the performance for one minute
 	printf("Source is not on output !");
-	std::this_thread::sleep_until(std::chrono::high_resolution_clock::now() + std::chrono::minutes(1));
+	std::this_thread::sleep_until(std::chrono::high_resolution_clock::now() + std::chrono::minutes(2));
 
 	// Destroy the NDI sender
 	NDIlib_send_destroy(pNDI_send);
