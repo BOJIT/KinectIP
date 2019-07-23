07bf63027878849169ad3280023d872e2608c5cd
James <BOJIT@users.noreply.github.com>
2019-07-23 23:14:19 +0100

    set permissions with script

--- cpp_core/build/script/BuildRunRemote.sh
+++ cpp_core/build/script/BuildRunRemote.sh
@@ -10,6 +10,7 @@ cd ./bin/aarch64_Linux
 scp KinectIP.aarch64.Linux rock64@192.168.1.150:
 cd ../..
 echo "-----------PROGRAM-----------"
+ssh rock64@192.168.1.150 './SetPermissions.exe'
 ssh rock64@192.168.1.150 './KinectIP.aarch64.Linux'
 exit
 umount /mnt/rock64
